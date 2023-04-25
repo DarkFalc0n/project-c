61e4c0ad02aa6fc01c5e0c98e2498d9bd0f93624
DarkFalc0n <unfezant211@gmail.com>
2023-04-25 21:13:49 +0530

    added loading bar and redirects

--- scenes/Loader.js
+++ scenes/Loader.js
@@ -2,13 +2,64 @@ import { Scene } from 'phaser';
 
 export default class Loader extends Scene {
     constructor() {
-        super('loading-scene');
+        super("Loader");
     }
     init() {
-        console.log("hello")
     }
     preload() {
+        //loading assets for scenes
         this.load.image('slime', 'assets/slime.png');
+        //-------------------------
+
+        //creating variables
+        this.scx = window.innerWidth; //screen center x
+        this.scy = window.innerHeight;
+        //--------------------------------
+
+        this.loadingText = this.add.text(
+            this.scx,
+            this.scy - 100,
+            "Loading",
+            {
+                fontFamily: 'Courier New',
+                fontSize: '64px',
+                fontStyle: 'bold',
+                fill: '#fff'
+            }
+        ).setOrigin(0.5);
+
+        this.add.rectangle(
+            this.scx,
+            this.scy,
+            1200,
+            50,
+            0
+        ).setStrokeStyle(10, 0xFFFFFF);
+
+        this.loadingBar = this.add.rectangle(
+            this.scx - 1170 / 2,
+            this.scy,
+            400,
+            20,
+            0xFFFFFF
+        ).setOrigin(0, 0.5)
+        // this.slime = this.physics.add.image(300, 300, "slime");
+
+        this.load.on("progress", (percent) => {
+            this.loadingBar.width = (1170 * percent);
+            this.loadingText.text = 'Loading ' + (percent * 100) + '%';
+        })
+
+        this.load.on("complete", () => {
+            setTimeout(() => {
+                this.scene.start("MainMenu")
+            }, 1000);
+        });
+
+        this.load.on("load", (file) => {
+            console.log(file.src)
+        })
+
     }
     create() {
         //setting up key names
@@ -19,66 +70,47 @@ export default class Loader extends Scene {
         this.downKey = this.keyControls.down;
         //-------------------------------------
 
-        //other variables
-        this.scx = window.innerWidth; //screen center x
-        this.scy = window.innerHeight;
-        //--------------------------------
-
-        this.add.text(
-            this.scx,
-            this.scy,
-            "Loading",
-            {
-                fontFamily: 'Roboto Mono',
-                fontSize: '64px',
-                fontStyle: 'bold',
-                fill: '#fff'
-            }
-        ).setOrigin(0.5);
-        // this.slime = this.physics.add.image(300, 300, "slime");
-
 
     }
     update() {
-        // this.movementManager()
     }
 
-    movementManager() {
-        //stopping conditions
-        if ((this.leftKey.isDown && this.rightKey.isDown) || (this.upKey.isDown && this.downKey.isDown)) {
-            this.stop();
-        }
-        if (this.leftKey.isUp || this.rightKey.isUp || this.upKey.isUp || this.downKey.isUp) {
-            this.stop();
-        }
-        //---------------------
+    // movementManager() {
+    //     //stopping conditions
+    //     if ((this.leftKey.isDown && this.rightKey.isDown) || (this.upKey.isDown && this.downKey.isDown)) {
+    //         this.stop();
+    //     }
+    //     if (this.leftKey.isUp || this.rightKey.isUp || this.upKey.isUp || this.downKey.isUp) {
+    //         this.stop();
+    //     }
+    //     //---------------------
 
-        var dir = {
-            x: 0,
-            y: 0
-        };
+    //     var dir = {
+    //         x: 0,
+    //         y: 0
+    //     };
 
-        //movement
-        if (this.leftKey.isDown === true) {
-            dir.x = -1;
-        }
-        else if (this.rightKey.isDown === true) {
-            dir.x = 1;
-        }
-        if (this.upKey.isDown === true) {
-            dir.y = -1;
-        }
-        else if (this.downKey.isDown === true) {
-            dir.y = 1;
-        }
-        this.move(dir);
-        //-------------------
-    }
-    stop() {
-        this.slime.setVelocity(0, 0);
-    }
-    move(dir) {  //dir is the direction of movement
-        const velocity = 300;
-        this.slime.setVelocity(dir.x * velocity, dir.y * velocity);
-    }
+    //     //movement
+    //     if (this.leftKey.isDown === true) {
+    //         dir.x = -1;
+    //     }
+    //     else if (this.rightKey.isDown === true) {
+    //         dir.x = 1;
+    //     }
+    //     if (this.upKey.isDown === true) {
+    //         dir.y = -1;
+    //     }
+    //     else if (this.downKey.isDown === true) {
+    //         dir.y = 1;
+    //     }
+    //     this.move(dir);
+    //     //-------------------
+    // }
+    // stop() {
+    //     this.slime.setVelocity(0, 0);
+    // }
+    // move(dir) {  //dir is the direction of movement
+    //     const velocity = 300;
+    //     this.slime.setVelocity(dir.x * velocity, dir.y * velocity);
+    // }
 }
